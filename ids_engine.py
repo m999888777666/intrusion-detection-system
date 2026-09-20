c9f7ab0cb102a6306fcf1ac0ddb840b6af43147a
melis <melissongurr@gmail.com>
2026-09-20 12:40:24 +0300

    Add IDS engine

--- ids_engine.py
+++ ids_engine.py
@@ -1,27 +1,25 @@
-from scapy.all import *
+from scapy.all import sniff
 from stats_manager import StatsManager
-stats=StatsManager() #yakalanan her paket aynı hafıza nesnesine işlenecek.
-def packet_callback(packet):
-    print(packet.summary()) #PAKET ÖZETİ
-    if IP in packet:
-        src_ip=packet[IP].src
-        dst_ip=packet[IP].dst
-        if TCP in packet:
-            src_port=packet[TCP].sport
-            dst_port=packet[TCP].dport
-            fag=packet[TCP].flags
-            cnt=stats.record_port(src_ip,dst_port)
-            if cnt>5:
-                print(f"[!] ALARM: Port scan detected from {src_ip}!")
-            if "S" in packet[TCP].flags:
-                if stats.record_syn(src_ip)>10:
-                    print(f"[!] ALARM: SYN Flood detected from {src_ip}!")
-            print(f"[TCP] {src_ip}:{src_port} -> {dst_ip}:{dst_port} | Flag: {fag}")
-        elif ICMP in packet:
-            icmp_type=packet[ICMP].type
-            if stats.record_icmp(src_ip)>10:
-                print(f"[!] ALARM: ICMP Flood detected from {src_ip}!")
+from alert_manager import AlertManager
+from detectors.syn_flood import SYNFloodDetector
+from detectors.port_scan import PortScanDetector
+from detectors.icmp_flood import ICMPFloodDetector
+class IDSEngine:
+    def __init__(self):
+        self.stats_manager=StatsManager()
+        self.alert_manager=AlertManager()
+        self.detectors=[
+            ICMPFloodDetector(self.stats_manager),
+            SYNFloodDetector(self.stats_manager),
+            PortScanDetector(self.stats_manager)
+        ]
+    def packet_callback(self, packet):
+        for detector in self.detectors:
+            alert=detector.detect(packet)
+            if alert:
+                self.alert_manager.process_alert(alert)
+    def start(self):
+        print("IDS has started...\n")
+        sniff(filter="ip",prn=self.packet_callback,store=0)
 
-            print(f"[ICMP] {src_ip} -> {dst_ip} | Type: {icmp_type}")
-sniff(filter="ip", prn=packet_callback)
-    
+
